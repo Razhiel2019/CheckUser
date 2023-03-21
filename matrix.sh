9d66a0bc8ddab8bf8e0dfdd39139c5bc9e17a550
Razhiel2019 <52635619+Razhiel2019@users.noreply.github.com>
2023-03-21 16:13:31 -0600

    Update matrix.sh

--- matrix.sh
+++ matrix.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 #MATRIX
 
-for i in (0..9999)
+for i in (0 9999)
 do
 echo -e "\e[1;32m 0 $ 1 8 0 & 1 0 B # 0 8 1 0 $ 1 8 0 B 0 $ 1 8 0 & 1 0 B # 0 8 1 0 $ 1 8 0 B \e[0m"
 echo -e "\e[1;32m  1 % 0 B 1 & 8 B   8 B 0 % 1   $ 0 B 0 1 I   & 1 B   0 B # 1   0 B 1 % 8 1  \e[0m"
